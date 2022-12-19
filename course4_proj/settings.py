cf055395f90643a65b537035edcfa5c7fcede654
TyomaKAV TyomaKAV <kompanijets@gmail.com>
2022-12-19 20:47:38 +0000

    Finish github library

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -78,6 +78,7 @@ class Dev(Configuration):
             'django.contrib.messages',
             'django.contrib.staticfiles',
             'movies',
+            'gh',
         ]
 
         # MIDDLEWARE = [
